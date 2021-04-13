f81b1181e8b0ae2916eae52202585201fc405a69
tsamantanis <philip.tsamantanis@icloud.com>
2021-04-13 09:02:51 -0700

    Make an openweather query using Zip

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const { buildSchema } = require('graphql')
+const fetch = require('node-fetch')
 require('dotenv').config()
 
 const schema = buildSchema(`
-    # schema here
-    type Test {
-        message: String!
+    type Weather {
+        temperature: Float!
+        description: String!
+    }
+
+    type Query {
+        getWeather(zip: Int!): Weather!
     }
 `)
 
 const root = {
-    // resolvers here
+    getWeather: async ({ zip }) => {
+        const url = `https://api.openweathermap.org/data/2.5/weather?zip=${ zip }&appid=${ process.env.OPENWEATHERMAP_API_KEY }`
+        const res = await fetch(url)
+        const json = await res.json()
+        const temperature = json.main.temp
+        const description = json.weather[0].description
+        return { temperature, description }
+    }
 }
 
 const app = express()
@@ -25,5 +37,5 @@ app.use('/graphql', graphqlHTTP({
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
-    console.log('Server running on port:' + port)
+    console.log('Server running on port: ' + port)
 })
