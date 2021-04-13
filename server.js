4fece5deb6bea7d9c09a176ce35946af5eca579d
tsamantanis <philip.tsamantanis@icloud.com>
2021-04-13 09:04:03 -0700

    Make a getWeather query using zip and units

--- server.js
+++ server.js
@@ -5,19 +5,24 @@ const fetch = require('node-fetch')
 require('dotenv').config()
 
 const schema = buildSchema(`
+    enum Units {
+        standard
+        metric
+        imperial
+    }
     type Weather {
         temperature: Float!
         description: String!
     }
 
     type Query {
-        getWeather(zip: Int!): Weather!
+        getWeather(zip: Int!, units: Units): Weather!
     }
 `)
 
 const root = {
-    getWeather: async ({ zip }) => {
-        const url = `https://api.openweathermap.org/data/2.5/weather?zip=${ zip }&appid=${ process.env.OPENWEATHERMAP_API_KEY }`
+    getWeather: async ({ zip, units = 'imperial' }) => {
+        const url = `https://api.openweathermap.org/data/2.5/weather?zip=${ zip }&appid=${ process.env.OPENWEATHERMAP_API_KEY }&units=${ units }`
         const res = await fetch(url)
         const json = await res.json()
         const temperature = json.main.temp
